edba4d99ce8ea06c36c0a030297a17321e48bbbb
Salazarj-debug <salazarj5102@student.faytechcc.edu>
2025-02-23 21:51:32 -0500

    Add files via upload

---
+++ P1LAB1_SalazarJustin.py
@@ -0,0 +1,12 @@
+#Justin Salazar
+#2/23/25
+#P1LAB1
+#Code that will use input from user to create message.
+print("Enter your first name:",end=' ')
+first_name= input()
+
+print("Enter your last name:",end=' ')
+Last_name= input()
+
+
+print("Hello,",first_name,Last_name,"! Welcome to CTI-110")
